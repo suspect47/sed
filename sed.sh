db268c517e0a740b701b032281483929949c60e6
Ilya Igonin <47140422+suspect47@users.noreply.github.com>
2021-11-10 14:37:06 +0300

    Update sed.sh

--- sed.sh
+++ sed.sh
@@ -1,2 +1,19 @@
 # обрезать последнюю часть пути по разделителю
 echo "repo/smart/speech/speechlab/speechlab:9.3.0.41316" | rev | sed -r 's/^[^/]+//' | rev
+
+
+#вытащить порт конкретного сервиса из docker-compose
+
+
+ fgg-streaming-service:
+    environment:
+      - Consul__Address=http://consul-server-bootstrap:8500/
+      - Consul__AppSettingsKey=config/demo
+      - Consul__ConfigurationKey=demo
+      - Consul__Enabled=true
+    ports:
+      - "9015:80"
+    image: test.com:5003/preprod/fgg-streaming-service:9.2.6.32488
+    restart: always
+
+grep -A 9 "fgg-streaming-service" docker-compose.yaml -m 1 | grep ":80" | awk {'print $2'} | sed -s "s/^\(\(\"\(.*\)\"\)\|\('\(.*\)'\)\)$/\3\5/g" | sed 's/:/ /g' | awk {'print $1'}
